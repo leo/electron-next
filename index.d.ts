0605f6fd18eb4b80d63450eaa45b10077a2a039a
Jerry Green <jerrygreenest@gmail.com>
2019-05-04 16:01:03 +0500

    handle object type for directories

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,16 @@
+type Dirs = string | string[]
+
+interface ComplexDirs {
+  development: Dirs,
+  production: Dirs
+}
+
 /**
  * Starts nextjs dev server (only in dev environment)
  * @param directories folder(s) where your next.config.js is placed
  * @param port on which port to run dev server (defaults to 8000)
  */
 export default function prepareRenderer (
-  directories: string | string[],
+  directories: Dirs | ComplexDirs,
   port?: string | number
 ): Promise<void>
